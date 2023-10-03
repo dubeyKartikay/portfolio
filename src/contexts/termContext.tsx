c01f1ab31e5876469d41636693468d7d53184f6f
Kartikay Dubey <kartikayd2003@gmail.com>
2023-10-03 14:15:48 +0530

    setup term Context

--- src/contexts/termContext.tsx
+++ src/contexts/termContext.tsx
@@ -1,6 +1,16 @@
-import {createContext} from "react";
-
-const TermContext = createContext(null);
-export default function TermContextProvider({children}:any){
-
+import {createContext, Dispatch, PropsWithChildren, useReducer} from "react";
+import { termlistInitState,termlistReducer, TTermDispatch, TTermList } from "../utils/termList";
+type TTermContext = {
+  termlist: TTermList,
+  termDispatch: Dispatch<TTermDispatch>
+}
+export const TermContext = createContext<TTermContext|null>(null)
+export default function TermContextProvider(props:PropsWithChildren){
+  const [termlist,termDispatch] = useReducer(termlistReducer,termlistInitState);
+  const termlistcontext:TTermContext = {termlist,termDispatch};
+  return(
+      <TermContext.Provider value={ termlistcontext }>
+        {props.children}
+      </TermContext.Provider>
+  )
 }
--- src/customHooks/useTermContext.tsx
+++ src/customHooks/useTermContext.tsx
@@ -1,18 +1,10 @@
-import {createContext, Dispatch, PropsWithChildren, useReducer} from "react";
-import { termlistInitState,termlistReducer, TTermDispatch, TTermList } from "../utils/termList";
-type TTermContext = {
-  termlist: TTermList,
-  termDispatch: Dispatch<TTermDispatch>
-}
-const TermContext = createContext<TTermContext|null>(null)
-export default function useTermContext(props:PropsWithChildren){
-  const [termlist,termDispatch] = useReducer(termlistReducer,termlistInitState);
-  const termlistcontext:TTermContext = {termlist,termDispatch};
-  return(
-    <>
-      <TermContext.Provider value={ termlistcontext }>
-        {props.children}
-      </TermContext.Provider>
-    </>  
-  )
+import { TermContext } from "../contexts/termContext";
+import { useContext } from "react";
+export default function useTermContext(){
+  const termContext = useContext(TermContext);
+  if(!termContext){
+    throw Error("Context must be used in a provider");
+  }
+  return termContext;
+
 }
