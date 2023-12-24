10011151e3272715572ff5ada2d5f43bdc1a4e55
Kartikay Dubey <kartikayd2003@gmail.com>
2023-12-24 15:54:04 +0530

    Updates

--- src/components/Terminal/TerminalEngine/TerminalEngine.tsx
+++ src/components/Terminal/TerminalEngine/TerminalEngine.tsx
@@ -1,7 +1,7 @@
 import { UserInput } from "./UserInput/UserInput"
 import { TerminalPrompt } from "./TerminalPrompt/TerminalPrompt"
 import { TerminalOut } from "./TerminalOut/TerminalOut"
-import { useState } from "react"
+import { useTermStateContext } from "../../../customHooks/useTermStateContext"
 export type TExchange = {
   prompt: typeof TerminalPrompt,
   input : typeof UserInput,
@@ -13,10 +13,16 @@ export type TTermState = {
   TermHist : TTermHist
 }
 export  const TerminalEngine = ()=> {
-  
+  const termStateContext = useTermStateContext();
   return (
     <div>
-
+      {termStateContext.termState.TermHist.map((ele)=>{
+        return <>
+          {ele.prompt}
+          {ele.input}
+          {ele.output}
+        </>
+      })} 
     </div>
   )
 }
